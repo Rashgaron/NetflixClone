68e7420a99056e2adbb24a4ad9028dcd56b5156a
Rashgaron <d.rodriguez@fruntera.com>
2022-12-17 00:31:13 +0100

    First commit

---
+++ Netflix Clone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,20 @@
+//
+//  HeroHeaderUIView.swift
+//  Netflix Clone
+//
+//  Created by Dani Rodriguez on 17/12/22.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
